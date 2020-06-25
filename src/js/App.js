995e2059a8aee7dadb57352c1f9e046ce0213c1c
Cazuist <sovrus@mail.ru>
2020-06-25 14:46:08 +0300

    First commit

---
+++ src/js/App.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import Game, {GameSavingData, readSavingData, writeGameSaving} from './Game.js';
+
+const game = new Game();
+
+game.start();
---
+++ src/js/Game.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import Character from './Domain.js';
+
+export default class Game {
+  start() {
+    console.log('game started!')
+  }
+}
+
+export class GameSavingData {
+}
+
+export function readSavingData() {
+}
+
+export function writeGameSaving() {
+}
